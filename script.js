b6f601a5af12441e3794336a50ae964258694fbc
Said Azmour <azmour2016maroc@gmail.com>
2024-02-02 16:42:44 +0100

    add js code

---
+++ script.js
@@ -0,0 +1,10 @@
+let icon = document.getElementById("icon");
+let ulNav = document.getElementById("ulnav");
+let li = document.querySelector("li");
+
+
+function changeUl() {
+  icon.style.display = "none";
+  ulNav.style.display = "block";
+}
+
